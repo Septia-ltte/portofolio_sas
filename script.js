b32b385afb50f5725324d7e23a39664f5f1d28ff
Septia <septiar436@gmail.com>
2025-12-06 11:31:31 +0700

    first commit

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-const form = document.querySelector('.contact-form');
+/*humbergur */
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
+
+hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
+});
+
+/*contact*/
+const form = document.querySelector('.contact-isi');
 const nama = document.querySelector('input[name="nama"]');
 const email = document.querySelector('input[name="email"]');
-const pesan = document.querySelector('textarea[name="pesan"]');
+const pesan = document.querySelector('textarea[name="message"]');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+form.addEventListener('submit', () => {
     alert('Thank you for your message!');
-    nama.value = '';
-    email.value = '';
-    pesan.value = '';
+
+    setTimeout(() => {
+        nama.value = '';
+        email.value = '';
+        pesan.value = '';
+    }, 150);
 });
